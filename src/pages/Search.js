5a47feb86f1f0c5ed46c937b39428de9707ebbb6
Rosa Nunez <146019823+RosaNunezRivera@users.noreply.github.com>
2024-06-16 14:23:41 -0500

    babel depedencies

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,5 +1,5 @@
 import { FaGithub } from 'react-icons/fa';
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect} from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom'
 import axios from 'axios';
 
